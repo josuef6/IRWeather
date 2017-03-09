d05c4a51dfcb827708984edf09c1cd79efe74ea1
josuef6 <josuef@g.clemson.edu>
2017-03-09 15:36:30 -0500

    app.js getRequest

--- app.js
+++ app.js
@@ -40,7 +40,13 @@ app.get('/process_get', function (req, res) {
         json: true
       },
       function (error, response, body) {
-       console.log(body)
+      	if(error){
+      		console.log(error);
+      	}
+      	else{
+      		res.send(body);
+      	}
+       //console.log(body)
       });
 })
 
